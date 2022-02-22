2fbac8a34158c00cd81a256e52db0047574bc0fa
Svetlana Smirnova <99610969+citrusssoda@users.noreply.github.com>
2022-02-22 12:25:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>C 23 февраля</title>  
+    <link href="woow.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="shapka1">
+  <center> <table height="auto" class="shapka3"><tr><td>
+
+<img src="https://marhal.ru/image/cache/catalog/sobitie/1-800x800.png" class="logo">
+</td><td>
+<h1 class="text1">День защитника Отечества</h1> <p class="nomer">ᅠ ᅠ23 февраля</p>
+</td></tr></table></center>
+    </div class="shapka1">
+<br><br>
+
+<div class="block">
+	<p>День защитника Отечества. 23 февраля в России отмечается День защитника Отечества, являющейся днем воинской славы страны. Праздник зародился еще в СССР; тогда 23 февраля ежегодно отмечался как всенародный праздник – День Советской Армии и Военно-Морского Флота.
+</p><br>
+<center> <img src="https://ds04.infourok.ru/uploads/ex/1047/00151ee6-f6ccb512/hello_html_m8882c2d.jpg" width="750"><br><br><img src="https://www.culture.ru/storage/images/17fd9ba740c5adcf6a5ba8aacdc50c3f/0dad76435dbb3ae630a8c52e7ce130fb.jpeg" width="750"></center>
+<br>
+<p>Праздник, отмечаемый ежегодно 23 февраля в Белоруссии, Киргизии, России и Таджикистане. Был установлен в РСФСР 27 января 1922 года, когда Президиум ВЦИК РСФСР опубликовал постановление о четвёртой годовщине Красной армии, в котором говорилось: «В соответствии с постановлением IX Всероссийского съезда Советов о Красной армии Президиум ВЦИК обращает внимание исполкомов на наступающую годовщину создания Красной армии».
+</p><br>
+</div>
+
+  <br><br><br>
+  <center><a class="info"> Смирнова С ИС-18 2022 </a><br></center>
+</body>
+</html>
